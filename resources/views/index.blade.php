19a28b8c3f43d2a24e3acb184a5c85e2f50f819d
meghana1974 <145559822+meghana1974@users.noreply.github.com>
2024-03-11 13:44:42 +0000

    Update index.blade.php to add movie cards feature

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -76,6 +76,60 @@
     <button class="prev-button">&#10094;</button>
     <button class="next-button">&#10095;</button>
 </div>
+<div class="movie_cards">
+    <div class="card_container" data-offset="2">
+        <div class="pg_1">
+          <img src="..\images\transparent-spider-man-spider-man-character-in-powerful-pose-wearing-span65cabb86615ac5.5423454717077850943988.png">
+        </div>
+        <div class="card" style="background:url(https://images7.alphacoders.com/943/thumb-1920-943918.jpg) 100% bottom; ">
+          <div class="shine"></div>
+          <div class="text-block">
+            <h1>SPIDER-MAN <small>(2002)</small></h1>
+            <h3>Action | Adventure</h3>
+            <p>
+                After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become Spider-Man. As he learns to harness his new-found abilities, Peter must face the challenge of balancing his personal life with his responsibility to fight crime in a bustling city, all while confronting a dark figure from his past—the nefarious Green Goblin.            </p>
+            <button>Watch Trailer</button>
+          </div>
+        </div>
+      </div>
+</div>
+
+      <div class="card_container" data-offset="2">
+        <div class="pg_1">
+          <img src="../images/pngwing.com.png">
+        </div>
+        <div class="card" style="background:url(https://w0.peakpx.com/wallpaper/426/791/HD-wallpaper-fast-8-the-fate-of-the-furious-fast-8-fast-and-furious-movies-2017-movies.jpg) 100% bottom; ">
+          <div class="shine"></div>
+          <div class="text-block">
+            <h1>FAST & FURIOUS 9 <small>(2021) </small></h1>
+            <h3>Action | Adventure</h3>
+            <p>
+                Dom Toretto confronts his past when his estranged brother, Jakob, an assassin allied with their enemy Cipher, threatens his family. Dom and his crew unite to stop Jakob's world-shattering plot in a high-stakes adventure that tests their limits and bonds.
+            </p>
+            <button>Watch Trailer</button>
+          </div>
+        </div>
+      </div>
+      
+    <div class="card_container" data-offset="2">
+        <div class="pg">
+          <img src="https://pluspng.com/img-png/aquaman-png-aquaman-png-clipart-666.png">
+        </div>
+        <div class="card" style="background:url(https://riverbeats.life/neworleans/wp-content/uploads/2018/07/Atlantis.png) 100% bottom; ">
+          <div class="shine"></div>
+          <div class="text-block">
+            <h1>Aquaman <small>(2018)</small></h1>
+            <h3>Action | Adventure</h3>
+            <p>
+              Arthur Curry learns that he is the heir to the underwater kingdom of Atlantis, and must step forward to lead his people and be a hero to the world.
+            </p>
+            <button>Watch Trailer</button>
+          </div>
+        </div>
+      </div>
+
+
+      
     <script>
         let slideIndex = 1;
         showSlides(slideIndex);
