f6ae5e5e4796ac8ff86c3b11899a0843ccf9e9ac
meghana1974 <145559822+meghana1974@users.noreply.github.com>
2024-03-12 03:25:32 +0000

    Create ContactController.php to handle contact form submissions and email notifications

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Mail;
+
+class ContactController extends Controller
+{
+    //
+    public function contact(){
+        return view('contact_us');
+    }
+    public function sendEmail(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255|regex:/^[a-zA-Z\s]+$/u',
+            'email' => 'required|email',
+            'phone' => 'required|regex:/^[0-9]{10,20}$/',
+            'msg' => 'required|string|max:1000',
+        ]);
+
+        $details =[
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'msg' => $request->msg
+
+        ];
+
+        Mail::to('D00262191@student.dkit.ie')->send(new ContactMail($details));
+        return back()->with('message_sent','Your message has been sent successfully!');
+    }
+}
